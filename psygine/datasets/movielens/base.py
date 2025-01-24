8a41bbd4df146788ca53adc62de5cb04a239d089
swolf <swolfforever@gmail.com>
2025-01-24 22:27:56 +0800

    Fix movielens merging error

--- psygine/datasets/movielens/base.py
+++ psygine/datasets/movielens/base.py
@@ -18,10 +18,8 @@ class BaseMovieLensDataset(BaseDataset):
         self.local_path = local_path
         self._table_cols = table_cols
         dests = self._data_path(local_path=self.local_path)
-        pd_tables = self._get_rawdata(dests)
-        self._pd_tables = pd.concat(
-            [pd_tables[table] for table in self.get_tables()], axis=1
-        )
+        self._raw_tables = self._get_rawdata(dests)
+        self._merge_tables = self._get_merged_tables(self._raw_tables)
 
     @abstractmethod
     def _data_path(self, local_path=None, force_update=False, proxies=None):
@@ -31,14 +29,21 @@ class BaseMovieLensDataset(BaseDataset):
     def _get_rawdata(self, dests):
         pass
 
+    @abstractmethod
+    def _get_merged_tables(self, raw_tables):
+        pass
+
     def get_columns(self, table):
         return self._table_cols[table]
 
     def get_tables(self):
         return self._table_cols.keys()
 
-    def get_rawdata(self):
-        return self._pd_tables
+    def get_rawtables(self):
+        return self._raw_tables
+
+    def get_mergedtables(self):
+        return self._merge_tables
 
     def __getitem__(self, idx):
-        return self._pd_tables.iloc[idx]
+        return self._merge_tables.iloc[idx]
--- psygine/datasets/movielens/movielens.py
+++ psygine/datasets/movielens/movielens.py
@@ -30,10 +30,68 @@ class MovieLensDataset_100K(BaseMovieLensDataset):
 
     Available tables include: data, item, and user.
 
+    Here are brief descriptions of the data.
+
+    ml-data.tar.gz   -- Compressed tar file.  To rebuild the u data files do this:
+                    gunzip ml-data.tar.gz
+                    tar xvf ml-data.tar
+                    mku.sh
+
+    u.data     -- The full u data set, 100000 ratings by 943 users on 1682 items.
+                  Each user has rated at least 20 movies.  Users and items are
+                  numbered consecutively from 1.  The data is randomly
+                  ordered. This is a tab separated list of
+                     user id | item id | rating | timestamp.
+                  The time stamps are unix seconds since 1/1/1970 UTC
+
+    u.info     -- The number of users, items, and ratings in the u data set.
+
+    u.item     -- Information about the items (movies); this is a tab separated
+                  list of
+                  movie id | movie title | release date | video release date |
+                  IMDb URL | unknown | Action | Adventure | Animation |
+                  Children's | Comedy | Crime | Documentary | Drama | Fantasy |
+                  Film-Noir | Horror | Musical | Mystery | Romance | Sci-Fi |
+                  Thriller | War | Western |
+                  The last 19 fields are the genres, a 1 indicates the movie
+                  is of that genre, a 0 indicates it is not; movies can be in
+                  several genres at once.
+                  The movie ids are the ones used in the u.data data set.
+
+    u.genre    -- A list of the genres.
+
+    u.user     -- Demographic information about the users; this is a tab
+                  separated list of
+                  user id | age | gender | occupation | zip code
+                  The user ids are the ones used in the u.data data set.
+
+    u.occupation -- A list of the occupations.
+
+    u1.base    -- The data sets u1.base and u1.test through u5.base and u5.test
+    u1.test       are 80%/20% splits of the u data into training and test data.
+    u2.base       Each of u1, ..., u5 have disjoint test sets; this if for
+    u2.test       5 fold cross validation (where you repeat your experiment
+    u3.base       with each training and test set and average the results).
+    u3.test       These data sets can be generated from u.data by mku.sh.
+    u4.base
+    u4.test
+    u5.base
+    u5.test
+
+    ua.base    -- The data sets ua.base, ua.test, ub.base, and ub.test
+    ua.test       split the u data into a training set and a test set with
+    ub.base       exactly 10 ratings per user in the test set.  The sets
+    ub.test       ua.test and ub.test are disjoint.  These data sets can
+                  be generated from u.data by mku.sh.
+
+    allbut.pl  -- The script that generates training and test sets where
+                  all but n of a users ratings are in the training data.
+
+    mku.sh     -- A shell script to generate all the u data sets from u.data.
     """
 
     __TABLE_COLS = {
-        "data": ["user id", "item id", "rating", "timestamp"],
+        "data": ["user id", "movie id", "rating"],
         "item": [
             "movie id",
             "movie title",
@@ -105,9 +163,11 @@ class MovieLensDataset_100K(BaseMovieLensDataset):
                 rawdata[table] = pd.read_csv(
                     dests[table],
                     sep="\t",
-                    names=self.get_columns(table),
+                    names=["user id", "item id", "rating", "timestamp"],
                     encoding="utf_8",
                 )
+                rawdata[table].rename(columns={"item id": "movie id"}, inplace=True)
+                rawdata[table].drop("timestamp", axis=1, inplace=True)
             elif table == "item":
                 rawdata[table] = pd.read_csv(
                     dests[table],
@@ -123,6 +183,15 @@ class MovieLensDataset_100K(BaseMovieLensDataset):
                     encoding="utf_8",
                 )
         return rawdata
+
+    def _get_merged_tables(self, raw_tables):
+        data = raw_tables["data"]
+        item = raw_tables["item"]
+        user = raw_tables["user"]
+
+        merged = pd.merge(data, user, how="outer", on="user id")
+        merged = pd.merge(merged, item, how="outer", on="movie id")
+        return merged
 
 
 class MovieLensDataset_1M(BaseMovieLensDataset):
@@ -231,7 +300,7 @@ class MovieLensDataset_1M(BaseMovieLensDataset):
     """
 
     __TABLE_COLS = {
-        "ratings": ["UserID", "MovieID", "Rating", "Timestamp"],
+        "ratings": ["UserID", "MovieID", "Rating"],
         "movies": ["MovieID", "Title", "Genres"],
         "users": ["UserID", "Gender", "Age", "Occupation", "Zip-code"],
     }
@@ -240,7 +309,7 @@ class MovieLensDataset_1M(BaseMovieLensDataset):
         super().__init__("ml-1m", self.__TABLE_COLS, local_path=local_path)
 
     def __len__(self):
-        return 1000209
+        return 1000386
 
     def _data_path(self, local_path=None, force_update=False, proxies=None):
         url = None
@@ -273,13 +342,31 @@ class MovieLensDataset_1M(BaseMovieLensDataset):
         tables = self.get_tables()
         rawdata = OrderedDict()
         for table in tables:
-            rawdata[table] = pd.read_csv(
-                dests[table],
-                sep="::",
-                names=self.get_columns(table),
-                encoding="latin_1" if table == "movies" else "utf_8",
-            )
+            if table == "ratings":
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep="::",
+                    names=["UserID", "MovieID", "Rating", "Timestamp"],
+                    encoding="latin_1" if table == "movies" else "utf_8",
+                )
+                rawdata[table].drop("Timestamp", axis=1, inplace=True)
+            else:
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep="::",
+                    names=self.get_columns(table),
+                    encoding="latin_1" if table == "movies" else "utf_8",
+                )
         return rawdata
+
+    def _get_merged_tables(self, raw_tables):
+        ratings = raw_tables["ratings"]
+        movies = raw_tables["movies"]
+        users = raw_tables["users"]
+
+        merged = pd.merge(ratings, users, how="outer", on="UserID")
+        merged = pd.merge(merged, movies, how="outer", on="MovieID")
+        return merged
 
 
 class MovieLensDataset_10M(BaseMovieLensDataset):
@@ -287,19 +374,80 @@ class MovieLensDataset_10M(BaseMovieLensDataset):
 
     Available tables include: ratings, movies, and tags.
 
+    User Ids
+
+    Movielens users were selected at random for inclusion. Their ids have been anonymized.
+
+    Users were selected separately for inclusion in the ratings and tags data sets, which implies that user ids may appear in one set but not the other.
+
+    The anonymized values are consistent between the ratings and tags data files. That is, user id n, if it appears in both files, refers to the same real MovieLens user.
+    Ratings Data File Structure
+
+    All ratings are contained in the file ratings.dat. Each line of this file represents one rating of one movie by one user, and has the following format:
+
+        UserID::MovieID::Rating::Timestamp
+
+    The lines within this file are ordered first by UserID, then, within user, by MovieID.
+
+    Ratings are made on a 5-star scale, with half-star increments.
+
+    Timestamps represent seconds since midnight Coordinated Universal Time (UTC) of January 1, 1970.
+    Tags Data File Structure
+
+    All tags are contained in the file tags.dat. Each line of this file represents one tag applied to one movie by one user, and has the following format:
+
+        UserID::MovieID::Tag::Timestamp
+
+    The lines within this file are ordered first by UserID, then, within user, by MovieID.
+
+    Tags are user generated metadata about movies. Each tag is typically a single word, or short phrase. The meaning, value and purpose of a particular tag is determined by each user.
+
+    Timestamps represent seconds since midnight Coordinated Universal Time (UTC) of January 1, 1970.
+    Movies Data File Structure
+
+    Movie information is contained in the file movies.dat. Each line of this file represents one movie, and has the following format:
+
+        MovieID::Title::Genres
+
+    MovieID is the real MovieLens id.
+
+    Movie titles, by policy, should be entered identically to those found in IMDB, including year of release. However, they are entered manually, so errors and inconsistencies may exist.
+
+    Genres are a pipe-separated list, and are selected from the following:
+
+        Action
+        Adventure
+        Animation
+        Children's
+        Comedy
+        Crime
+        Documentary
+        Drama
+        Fantasy
+        Film-Noir
+        Horror
+        Musical
+        Mystery
+        Romance
+        Sci-Fi
+        Thriller
+        War
+        Western
+
+
     """
 
     __TABLE_COLS = {
-        "ratings": ["UserID", "MovieID", "Rating", "Timestamp"],
+        "ratings": ["UserID", "MovieID", "Rating"],
         "movies": ["MovieID", "Title", "Genres"],
-        "tags": ["UserID", "MovieID", "Tag", "Timestamp"],
+        "tags": ["UserID", "MovieID", "Tag"],
     }
 
     def __init__(self, local_path=None):
         super().__init__("ml-10m", self.__TABLE_COLS, local_path=local_path)
 
     def __len__(self):
-        return 10000054
+        return 10000058
 
     def _data_path(self, local_path=None, force_update=False, proxies=None):
         url = None
@@ -333,13 +481,39 @@ class MovieLensDataset_10M(BaseMovieLensDataset):
 
         rawdata = OrderedDict()
         for table in tables:
-            rawdata[table] = pd.read_csv(
-                dests[table],
-                sep="::",
-                names=self.get_columns(table),
-                encoding="utf_8",
-            )
+            if table == "ratings":
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep="::",
+                    names=["UserID", "MovieID", "Rating", "Timestamp"],
+                    encoding="utf_8",
+                )
+                rawdata[table].drop("Timestamp", axis=1, inplace=True)
+            elif table == "tags":
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep="::",
+                    names=["UserID", "MovieID", "Tag", "Timestamp"],
+                    encoding="utf_8",
+                )
+                rawdata[table].drop("Timestamp", axis=1, inplace=True)
+            else:
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep="::",
+                    names=self.get_columns(table),
+                    encoding="utf_8",
+                )
         return rawdata
+
+    def _get_merged_tables(self, raw_tables):
+        ratings = raw_tables["ratings"]
+        movies = raw_tables["movies"]
+        # tags = raw_tables["tags"]
+
+        merged = pd.merge(ratings, movies, how="outer", on="MovieID")
+        # merged = pd.merge(merged, tags, how="outer", on=["UserID", "MovieID"])
+        return merged
 
 
 class MovieLensDataset_20M(BaseMovieLensDataset):
@@ -347,12 +521,117 @@ class MovieLensDataset_20M(BaseMovieLensDataset):
 
     Available tables include: ratings, movies, tags, links, genome-scores, and genome-tags.
 
+    The dataset files are written as [comma-separated values](http://en.wikipedia.org/wiki/Comma-separated_values) files with a single header row. Columns that contain commas (`,`) are escaped using double-quotes (`"`). These files are encoded as UTF-8. If accented characters in movie titles or tag values (e.g. Misérables, Les (1995)) display incorrectly, make sure that any program reading the data, such as a text editor, terminal, or script, is configured for UTF-8.
+
+    User Ids
+    --------
+
+    MovieLens users were selected at random for inclusion. Their ids have been anonymized. User ids are consistent between `ratings.csv` and `tags.csv` (i.e., the same id refers to the same user across the two files).
+
+    Movie Ids
+    ---------
+
+    Only movies with at least one rating or tag are included in the dataset. These movie ids are consistent with those used on the MovieLens web site (e.g., id `1` corresponds to the URL <https://movielens.org/movies/1>). Movie ids are consistent between `ratings.csv`, `tags.csv`, `movies.csv`, and `links.csv` (i.e., the same id refers to the same movie across these four data files).
+
+
+    Ratings Data File Structure (ratings.csv)
+    -----------------------------------------
+
+    All ratings are contained in the file `ratings.csv`. Each line of this file after the header row represents one rating of one movie by one user, and has the following format:
+
+        userId,movieId,rating,timestamp
+
+    The lines within this file are ordered first by userId, then, within user, by movieId.
+
+    Ratings are made on a 5-star scale, with half-star increments (0.5 stars - 5.0 stars).
+
+    Timestamps represent seconds since midnight Coordinated Universal Time (UTC) of January 1, 1970.
+
+    Tags Data File Structure (tags.csv)
+    -----------------------------------
+
+    All tags are contained in the file `tags.csv`. Each line of this file after the header row represents one tag applied to one movie by one user, and has the following format:
+
+        userId,movieId,tag,timestamp
+
+    The lines within this file are ordered first by userId, then, within user, by movieId.
+
+    Tags are user-generated metadata about movies. Each tag is typically a single word or short phrase. The meaning, value, and purpose of a particular tag is determined by each user.
+
+    Timestamps represent seconds since midnight Coordinated Universal Time (UTC) of January 1, 1970.
+
+    Movies Data File Structure (movies.csv)
+    ---------------------------------------
+
+    Movie information is contained in the file `movies.csv`. Each line of this file after the header row represents one movie, and has the following format:
+
+        movieId,title,genres
+
+    Movie titles are entered manually or imported from <https://www.themoviedb.org/>, and include the year of release in parentheses. Errors and inconsistencies may exist in these titles.
+
+    Genres are a pipe-separated list, and are selected from the following:
+
+    * Action
+    * Adventure
+    * Animation
+    * Children's
+    * Comedy
+    * Crime
+    * Documentary
+    * Drama
+    * Fantasy
+    * Film-Noir
+    * Horror
+    * Musical
+    * Mystery
+    * Romance
+    * Sci-Fi
+    * Thriller
+    * War
+    * Western
+    * (no genres listed)
+
+    Links Data File Structure (links.csv)
+    ---------------------------------------
+
+    Identifiers that can be used to link to other sources of movie data are contained in the file `links.csv`. Each line of this file after the header row represents one movie, and has the following format:
+
+        movieId,imdbId,tmdbId
+
+    movieId is an identifier for movies used by <https://movielens.org>. E.g., the movie Toy Story has the link <https://movielens.org/movies/1>.
+
+    imdbId is an identifier for movies used by <http://www.imdb.com>. E.g., the movie Toy Story has the link <http://www.imdb.com/title/tt0114709/>.
+
+    tmdbId is an identifier for movies used by <https://www.themoviedb.org>. E.g., the movie Toy Story has the link <https://www.themoviedb.org/movie/862>.
+
+    Use of the resources listed above is subject to the terms of each provider.
+
+    Tag Genome (genome-scores.csv and genome-tags.csv)
+    -------------------------------------------------
+
+    This data set includes a current copy of the Tag Genome.
+
+    [genome-paper]: http://files.grouplens.org/papers/tag_genome.pdf
+
+    The tag genome is a data structure that contains tag relevance scores for movies.  The structure is a dense matrix: each movie in the genome has a value for *every* tag in the genome.
+
+    As described in [this article][genome-paper], the tag genome encodes how strongly movies exhibit particular properties represented by tags (atmospheric, thought-provoking, realistic, etc.). The tag genome was computed using a machine learning algorithm on user-contributed content including tags, ratings, and textual reviews.
+
+    The genome is split into two files.  The file `genome-scores.csv` contains movie-tag relevance data in the following format:
+
+        movieId,tagId,relevance
+
+    The second file, `genome-tags.csv`, provides the tag descriptions for the tag IDs in the genome file, in the following format:
+
+        tagId,tag
+
+    The `tagId` values are generated when the data set is exported, so they may vary from version to version of the MovieLens data sets.
     """
 
     __TABLE_COLS = {
-        "ratings": ["userId", "movieId", "rating", "timestamp"],
+        "ratings": ["userId", "movieId", "rating"],
         "movies": ["movieId", "title", "genres"],
-        "tags": ["userId", "movieId", "tag", "timestamp"],
+        "tags": ["userId", "movieId", "tag"],
         "links": ["movieId", "imdbId", "tmdbId"],
         "genome-scores": ["movieId", "tagId", "relevance"],
         "genome-tags": ["tagId", "tag"],
@@ -362,7 +641,7 @@ class MovieLensDataset_20M(BaseMovieLensDataset):
         super().__init__("ml-20m", self.__TABLE_COLS, local_path=local_path)
 
     def __len__(self):
-        return 20000263
+        return 20000797
 
     def _data_path(self, local_path=None, force_update=False, proxies=None):
         url = None
@@ -396,11 +675,42 @@ class MovieLensDataset_20M(BaseMovieLensDataset):
 
         rawdata = OrderedDict()
         for table in tables:
-            rawdata[table] = pd.read_csv(
-                dests[table],
-                sep=",",
-                header=0,
-                names=self.get_columns(table),
-                encoding="utf_8",
-            )
+            if table == "ratings":
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep=",",
+                    header=0,
+                    names=["userId", "movieId", "rating", "timestamp"],
+                    encoding="utf_8",
+                )
+                rawdata[table].drop("timestamp", axis=1, inplace=True)
+            elif table == "tags":
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep=",",
+                    header=0,
+                    names=["userId", "movieId", "tag", "timestamp"],
+                    encoding="utf_8",
+                )
+                rawdata[table].drop("timestamp", axis=1, inplace=True)
+            else:
+                rawdata[table] = pd.read_csv(
+                    dests[table],
+                    sep=",",
+                    header=0,
+                    names=self.get_columns(table),
+                    encoding="utf_8",
+                )
         return rawdata
+
+    def _get_merged_tables(self, raw_tables):
+        ratings = raw_tables["ratings"]
+        movies = raw_tables["movies"]
+        # tags = raw_tables["tags"]
+        # links = raw_tables["links"]
+        # genome_scores = raw_tables["genome-scores"]
+        # genome_tags = raw_tables["genome-tags"]
+
+        merged = pd.merge(ratings, movies, how="outer", on="movieId")
+        # merged = pd.merge(merged, tags, how="outer", on=["userId", "movieId"])
+        return merged
