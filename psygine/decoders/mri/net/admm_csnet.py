d7f106330473742ed687747f65d4aa9428288657
swolf <swolfforever@gmail.com>
2025-03-18 22:24:46 +0800

    FIX: Fix ADMMCSNet bugs; NEW: Add an efficient pwl module; Add the original ADMM CSNet;

--- psygine/decoders/mri/net/admm_csnet.py
+++ psygine/decoders/mri/net/admm_csnet.py
@@ -13,13 +13,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 __all__ = [
-    "PWL",
-    "MonoPWL",
-    "ReconLayer",
-    "MultiplierLayer",
-    "ProximalLayer",
     "ADMMCSNet",
-    "MyPWL",
+    "ADMMCSNetOri",
 ]
 
 
@@ -429,35 +424,322 @@ class MonoPWL(PWL):
         )
 
 
-class MyPWL(nn.Module):
+class ADMMCSNetOri(nn.Module):
+    def __init__(
+        self,
+        mask,
+        in_channels=1,
+        out_channels=128,
+        kernel_size=5,
+        num_breakpoints=101,
+    ):
+        super().__init__()
+
+        self.rho = nn.Parameter(torch.tensor([0.1]), requires_grad=True)
+        self.gamma = nn.Parameter(torch.tensor([1.0]), requires_grad=True)
+        self.register_buffer("mask", torch.tensor(mask, dtype=torch.float32))
+        self.re_org_layer = ReconstructionOriginalLayer(self.rho, self.mask)
+        self.conv1_layer = ConvolutionLayer1(in_channels, out_channels, kernel_size)
+        self.nonlinear_layer = NonlinearLayer(out_channels, num_breakpoints)
+        self.conv2_layer = ConvolutionLayer2(out_channels, in_channels, kernel_size)
+        self.min_layer = MinusLayer()
+        self.multiple_org_layer = MultipleOriginalLayer(self.gamma)
+        self.re_update_layer = ReconstructionUpdateLayer(self.rho, self.mask)
+        self.add_layer = AdditionalLayer()
+        self.multiple_update_layer = MultipleUpdateLayer(self.gamma)
+        self.re_final_layer = ReconstructionFinalLayer(self.rho, self.mask)
+        layers = []
+
+        layers.append(self.re_org_layer)
+        layers.append(self.conv1_layer)
+        layers.append(self.nonlinear_layer)
+        layers.append(self.conv2_layer)
+        layers.append(self.min_layer)
+        layers.append(self.multiple_org_layer)
+
+        for i in range(8):
+            layers.append(self.re_update_layer)
+            layers.append(self.add_layer)
+            layers.append(self.conv1_layer)
+            layers.append(self.nonlinear_layer)
+            layers.append(self.conv2_layer)
+            layers.append(self.min_layer)
+            layers.append(self.multiple_update_layer)
+
+        layers.append(self.re_update_layer)
+        layers.append(self.add_layer)
+        layers.append(self.conv1_layer)
+        layers.append(self.nonlinear_layer)
+        layers.append(self.conv2_layer)
+        layers.append(self.min_layer)
+        layers.append(self.multiple_update_layer)
+
+        layers.append(self.re_final_layer)
+
+        self.cs_net = nn.Sequential(*layers)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.conv1_layer.conv.weight = torch.nn.init.normal_(
+            self.conv1_layer.conv.weight, mean=0, std=1
+        )
+        self.conv2_layer.conv.weight = torch.nn.init.normal_(
+            self.conv2_layer.conv.weight, mean=0, std=1
+        )
+        self.conv1_layer.conv.weight.data = self.conv1_layer.conv.weight.data * 0.025
+        self.conv2_layer.conv.weight.data = self.conv2_layer.conv.weight.data * 0.025
+
+    def forward(self, x):
+        y = torch.mul(x, self.mask)
+        x = self.cs_net(y)
+        x = torch.fft.ifft2(y + (1 - self.mask) * torch.fft.fft2(x))
+        return x
+
+
+# reconstruction original layers
+class ReconstructionOriginalLayer(nn.Module):
+    def __init__(self, rho, mask):
+        super(ReconstructionOriginalLayer, self).__init__()
+        self.rho = rho
+        self.mask = mask
+
+    def forward(self, x):
+        mask = self.mask
+        denom = torch.add(mask.to(device=x.device), self.rho)
+        a = 1e-6
+        value = torch.full(denom.size(), a).to(device=x.device)
+        denom = torch.where(denom == 0, value, denom)
+        orig_output1 = torch.div(1, denom)
+
+        orig_output2 = torch.mul(x, orig_output1)
+        orig_output3 = torch.fft.ifft2(orig_output2)
+        # define data dict
+        cs_data = dict()
+        cs_data["input"] = x
+        cs_data["conv1_input"] = orig_output3
+        return cs_data
+
+
+# reconstruction middle layers
+class ReconstructionUpdateLayer(nn.Module):
+    def __init__(self, rho, mask):
+        super(ReconstructionUpdateLayer, self).__init__()
+        self.rho = rho
+        self.mask = mask
+
+    def forward(self, x):
+        minus_output = x["minus_output"]
+        multiple_output = x["multi_output"]
+        input = x["input"]
+        mask = self.mask
+        number = torch.add(
+            input, self.rho * torch.fft.fft2(torch.sub(minus_output, multiple_output))
+        )
+        denom = torch.add(mask.to(device=input.device), self.rho)
+        a = 1e-6
+        value = torch.full(denom.size(), a).to(device=input.device)
+        denom = torch.where(denom == 0, value, denom)
+        orig_output1 = torch.div(1, denom)
+        orig_output2 = torch.mul(number, orig_output1)
+        orig_output3 = torch.fft.ifft2(orig_output2)
+        x["re_mid_output"] = orig_output3
+        return x
+
+
+# reconstruction middle layers
+class ReconstructionFinalLayer(nn.Module):
+    def __init__(self, rho, mask):
+        super(ReconstructionFinalLayer, self).__init__()
+        self.rho = rho
+        self.mask = mask
+
+    def forward(self, x):
+        minus_output = x["minus_output"]
+        multiple_output = x["multi_output"]
+        input = x["input"]
+        mask = self.mask
+        number = torch.add(
+            input, self.rho * torch.fft.fft2(torch.sub(minus_output, multiple_output))
+        )
+        denom = torch.add(mask.to(device=input.device), self.rho)
+        a = 1e-6
+        value = torch.full(denom.size(), a).to(device=input.device)
+        denom = torch.where(denom == 0, value, denom)
+        orig_output1 = torch.div(1, denom)
+        orig_output2 = torch.mul(number, orig_output1)
+        orig_output3 = torch.fft.ifft2(orig_output2)
+        x["re_final_output"] = orig_output3
+        return x["re_final_output"]
+
+
+# multiple original layer
+class MultipleOriginalLayer(nn.Module):
+    def __init__(self, gamma):
+        super(MultipleOriginalLayer, self).__init__()
+        self.gamma = gamma
+
+    def forward(self, x):
+        org_output = x["conv1_input"]
+        minus_output = x["minus_output"]
+        output = torch.mul(self.gamma, torch.sub(org_output, minus_output))
+        x["multi_output"] = output
+        return x
+
+
+# multiple middle layer
+class MultipleUpdateLayer(nn.Module):
+    def __init__(self, gamma):
+        super(MultipleUpdateLayer, self).__init__()
+        self.gamma = gamma
+
+    def forward(self, x):
+        multiple_output = x["multi_output"]
+        re_mid_output = x["re_mid_output"]
+        minus_output = x["minus_output"]
+        output = torch.add(
+            multiple_output,
+            torch.mul(self.gamma, torch.sub(re_mid_output, minus_output)),
+        )
+        x["multi_output"] = output
+        return x
+
+
+# convolution layer
+class ConvolutionLayer1(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int):
+        super(ConvolutionLayer1, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=int((kernel_size - 1) / 2),
+            stride=1,
+            dilation=1,
+            bias=True,
+        )
+
+    def forward(self, x):
+        conv1_input = x["conv1_input"]
+        real = self.conv(conv1_input.real)
+        imag = self.conv(conv1_input.imag)
+        output = torch.complex(real, imag)
+        x["conv1_output"] = output
+        return x
+
+
+# convolution layer
+class ConvolutionLayer2(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int):
+        super(ConvolutionLayer2, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=int((kernel_size - 1) / 2),
+            stride=1,
+            dilation=1,
+            bias=True,
+        )
+
+    def forward(self, x):
+        nonlinear_output = x["nonlinear_output"]
+        real = self.conv(nonlinear_output.real)
+        imag = self.conv(nonlinear_output.imag)
+        output = torch.complex(real, imag)
+
+        x["conv2_output"] = output
+        return x
+
+
+# nonlinear layer
+class NonlinearLayer(nn.Module):
+    def __init__(self, in_channels, num_breakpoints):
+        super(NonlinearLayer, self).__init__()
+        # self.pwl = PWL(num_channels=in_channels, num_breakpoints=num_breakpoints)
+        self.pwl = EfficientPWL(in_channels, num_breakpoints)
+
+    def forward(self, x):
+        conv1_output = x["conv1_output"]
+        y_real = self.pwl(conv1_output.real)
+        y_imag = self.pwl(conv1_output.imag)
+        output = torch.complex(y_real, y_imag)
+        x["nonlinear_output"] = output
+        return x
+
+
+# minus layer
+class MinusLayer(nn.Module):
+    def __init__(self):
+        super(MinusLayer, self).__init__()
+
+    def forward(self, x):
+        minus_input = x["conv1_input"]
+        conv2_output = x["conv2_output"]
+        output = torch.sub(minus_input, conv2_output)
+        x["minus_output"] = output
+        return x
+
+
+# addtional layer
+class AdditionalLayer(nn.Module):
+    def __init__(self):
+        super(AdditionalLayer, self).__init__()
+
+    def forward(self, x):
+        mid_output = x["re_mid_output"]
+        multi_output = x["multi_output"]
+        output = torch.add(mid_output, multi_output)
+        x["conv1_input"] = output
+        return x
+
+
+class EfficientPWL(nn.Module):
     def __init__(self, in_channels, num_cpnts):
         super().__init__()
         self.num_cpnts = num_cpnts
-        self.register_buffer("delta_t", torch.tensor([1 / (num_cpnts - 1)]))
+        self.register_buffer("delta_t", torch.tensor([2 / (num_cpnts - 1)]))
         self.register_buffer("cpnts", torch.linspace(-1, 1, steps=num_cpnts))
         self.slopes = nn.Parameter(
-            torch.randn(in_channels, num_cpnts + 1), requires_grad=True
+            torch.Tensor(in_channels, num_cpnts + 1), requires_grad=True
         )
-        self.bias = nn.Parameter(torch.randn(in_channels), requires_grad=True)
+        self.biases = nn.Parameter(torch.Tensor(in_channels), requires_grad=True)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        # torch.nn.init.normal_(self.slopes)
+        # torch.nn.init.normal_(self.biases)
+        torch.nn.init.ones_(self.slopes)
+        self.slopes.data[:, : (self.num_cpnts + 1) // 2] = 0.0
+        torch.nn.init.zeros_(self.biases)
 
     def forward(self, x):
+        with torch.no_grad():
+            ind1 = torch.searchsorted(self.cpnts, x, side="right")
+            ind2 = ind1 - 1
+            ind2[ind2 < 0] = 0
+
         cumbias = torch.cumsum(self.slopes[:, 1:-1] * self.delta_t, dim=1)
         cumbias = torch.cat(
-            (torch.zeros(cumbias.shape[0], 1, device=cumbias.device), cumbias), dim=1
+            [self.biases.view(-1, 1), cumbias + self.biases.view(-1, 1)], dim=1
         )
-        cumbias += self.bias.view(-1, 1)
 
-        ind = torch.searchsorted(self.cpnts, x, side="right")
-        selected_slopes = torch.gather(
-            self.slopes.expand(x.shape[0], -1, -1), 2, ind.view(*x.shape[:2], -1)
+        cpnts = self.cpnts[ind2]
+        slopes = torch.gather(
+            self.slopes.unsqueeze(0).expand(x.shape[0], -1, -1),
+            2,
+            ind1.view(*x.shape[:2], -1),
         ).view(*x.shape)
-        ind = ind - 1
-        ind[ind < 0] = 0
-        selected_cpnts = self.cpnts[ind]
-        selected_bias = torch.gather(
-            cumbias.expand(x.shape[0], -1, -1), 2, ind.view(*x.shape[:2], -1)
+        biases = torch.gather(
+            cumbias.unsqueeze(0).expand(x.shape[0], -1, -1),
+            2,
+            ind2.view(*x.shape[:2], -1),
         ).view(*x.shape)
-        x = (x - selected_cpnts) * selected_slopes + selected_bias
+        x = (x - cpnts) * slopes + biases
         return x
 
 
@@ -480,7 +762,7 @@ class ReconLayer(nn.Module):
             x: (B, 1, H, W)
         """
         if z is not None or beta is not None:
-            b = mask * y + self.rho * torch.fft.fft2(z - beta, norm="ortho")
+            b = mask * y + self.rho * torch.fft.fft2(z - beta)
         else:
             b = mask * y
 
@@ -489,7 +771,7 @@ class ReconLayer(nn.Module):
         # (rho*I + S)^-1 * S^H*y
         x = torch.where(denom != 0, b, 0) / torch.where(denom != 0, denom, 1)
         # F^H*(rho*I + S)^-1 * S^H*y
-        x = torch.fft.ifft2(x, norm="ortho")
+        x = torch.fft.ifft2(x)
         return x
 
 
@@ -520,9 +802,6 @@ class ProximalLayer(nn.Module):
         self.miu1 = nn.Parameter(
             torch.tensor([0.5], dtype=torch.float32), requires_grad=True
         )
-        self.miu2 = nn.Parameter(
-            torch.tensor([0.5], dtype=torch.float32), requires_grad=True
-        )
         self.conv1 = nn.Conv2d(
             in_chan,
             L,
@@ -537,8 +816,7 @@ class ProximalLayer(nn.Module):
             stride=1,
             padding=kernel_size // 2,
         )
-        # self.nonlinear_layer = PWL(L, num_breakpoints=num_breakpoints)
-        self.nonlinear_layer = MyPWL(L, num_breakpoints)
+        self.nonlinear_layer = EfficientPWL(L, num_breakpoints)
 
     def forward(self, z):
         z0 = z.clone()
@@ -553,7 +831,7 @@ class ProximalLayer(nn.Module):
             imag = self.conv2(imag)
 
             c = torch.complex(real, imag)
-            z = self.miu1 * z + self.miu2 * z0 - c
+            z = self.miu1 * z + (1 - self.miu1) * z0 - c
 
         return z
 
@@ -597,7 +875,5 @@ class ADMMCSNet(nn.Module):
             beta = self.multiplier_layer(x, z, beta)
 
         x = self.recon_layer(self.mask, y, z, beta)
-        x = torch.fft.ifft2(
-            y + (1 - self.mask) * torch.fft.fft2(x, norm="ortho"), norm="ortho"
-        )
+        x = torch.fft.ifft2(y + (1 - self.mask) * torch.fft.fft2(x))
         return x
